cc36e716030cdfc59a11dfda6ce39480bef15537
Evan-Marier <emarier27@cranbrook.edu>
2025-03-07 18:10:32 -0500

    Update Elevator.java

--- Elevator.java
+++ Elevator.java
@@ -1,2 +1,28 @@
-public class Elevator {
+package frc.robot;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase{
+    private static Elevator instance = null;
+    private final SparkMax driveMotorL;
+    private final SparkMax driveMotorR;
+    private final SparkMax wristMotor;
+    private final SparkMax intakeMotor;
+    
+    
+    private Elevator() {
+        driveMotorL = new SparkMax(5, MotorType.kBrushless);
+        driveMotorR = new SparkMax(6, MotorType.kBrushless);
+        wristMotor = new SparkMax(7, MotorType.kBrushless);
+        intakeMotor = new SparkMax(8, MotorType.kBrushless);
+    }    
+    
+    public static Elevator getInstance() {
+        if(instance == null) 
+            instance = new Elevator();
+        return instance;
+    }
 }
