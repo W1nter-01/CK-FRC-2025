361ca2a01675a760aae19c4e0b85a10ae7e42e28
Evan-Marier <emarier27@cranbrook.edu>
2025-03-08 11:41:49 -0500

    Update OperatorInterface.java

--- OperatorInterface.java
+++ OperatorInterface.java
@@ -20,12 +20,22 @@ public class OperatorInterface extends SubsystemBase {
         Drivetrain Drive = Drivetrain.getInstance();
         double left = driverCTRL.getRawAxis(1);
         double right = driverCTRL.getRawAxis(5);
-        double rightE = driverCTRL.getRawAxis(4);
-        boolean pressJoyL = driverCTRL.getRawButton(8);
-        boolean pressJoyR = driverCTRL.getRawButton(9);
+        double rightE = driverCTRL.getRawAxis(2);
+        boolean pressJoyL = driverCTRL.getRawButton(10);
+        boolean pressJoyR = driverCTRL.getRawButton(11);
+
+        boolean pressSquare = driverCTRL.getRawButton(0);
+        boolean pressX = driverCTRL.getRawButton(1);
+        boolean pressCircle = driverCTRL.getRawButton(2);
+        boolean pressTriangle = driverCTRL.getRawButton(3);
+        boolean pressR1 = driverCTRL.getRawButton(5);
+        boolean pressR2 = driverCTRL.getRawButton(7);
+        boolean pressOptions = driverCTRL.getRawButton(8);
+        boolean pressShare = driverCTRL.getRawButton(9);
+        boolean pressL1 = driverCTRL.getRawButton(4);
+
         Drive.setMotor(left, right, rightE, pressJoyL, pressJoyR);
-        boolean xButton = driverCTRL.getRawButton(0);
-        
+        (pressSquare, pressX, pressCircle, pressTriangle, pressR1, pressR2, pressOptions, pressShare, pressL1);
     }
 
     public static OperatorInterface getInstance(){
