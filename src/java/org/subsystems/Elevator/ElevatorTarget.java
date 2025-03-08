72dd9dfacf53bd1e8579d34d97eeaeef9fa2e5b8
W1nter-01 <martinhuang070909@gmail.com>
2025-03-08 12:39:53 -0500

    Update ElevatorTarget.java

--- src/java/org/subsystems/Elevator/ElevatorTarget.java
+++ src/java/org/subsystems/Elevator/ElevatorTarget.java
@@ -1,4 +1,11 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import java.org.subsystems.Elevator;
+
 public class ElevatorTarget () extends Command{
+    public final Elevator elevator;
+    
     public elevatorTarget {
     }
 
